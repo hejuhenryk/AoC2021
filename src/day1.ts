3f4910571dac1e631e158ac768c3dabc90a7505e
Marcin Sawczuk-Szymkiewicz <sawczuk.marcin@gmail.com>
2021-12-01 21:55:27 +0100

    da1 solusion

--- src/day1.ts
+++ src/day1.ts
@@ -1,29 +1,26 @@
+import { appendResault, transformInputToArrayOfNumbers } from "./utils.js";
+
+
+const day1part1 = (input: number[]): number => input.reduce((acc, curr, indx, arr) => {
+  if (indx > 0) {
+    return curr > arr[indx - 1] ? ++acc : acc
+  } else return 0
+}, 0)
+const day1part2 = (input: number[]): number => input.reduce((acc, curr, indx, arr) => {
+  if (indx > 2) {
+    return arr[indx] + arr[indx - 1] + arr[indx - 2] > arr[indx - 1] + + arr[indx - 2] + arr[indx - 3] ? ++acc : acc
+  } else return 0
+}, 0)
+
+
 fetch("../dist/inputs/input1.txt")
   .then((res) => res.text())
-  .then((res) => console.log(sumThreeTo2020(res.split("\n").map((n) => +n))));
+  .then((res) => transformInputToArrayOfNumbers(res))
+  .then((res) => {
+    appendResault(day1part1(res), 1, 1)
+    return res
+  })
+  .then((res) => {
+    appendResault(day1part2(res), 1, 2)
+  })
 
-const sumTwoTo2020 = (data: number[]) => {
-  let startIndx = 0;
-  let endIndx = data.length - 1;
-  const getSum = () => data[startIndx] + data[endIndx];
-  data.sort((a, b) => a - b);
-  while (getSum() !== 2020 && startIndx < endIndx) {
-    if (getSum() < 2020) startIndx++;
-    else if (getSum() > 2020) endIndx--;
-  }
-  return getSum() === 2020 ? data[startIndx] * data[endIndx] : undefined;
-};
-
-const sumThreeTo2020 = (data: number[]) => {
-  let resaut: undefined | number = undefined;
-  data = data.sort((a, b) => a - b);
-  console.log(data);
-  const restDict: { [k: number]: number } = {};
-  data.forEach((x) => {
-    data.forEach((y) => (restDict[x + y] = x * y));
-  });
-  data.forEach((x) => {
-    if (restDict[2020 - x]) resaut = x * restDict[2020 - x];
-  });
-  return resaut;
-};
